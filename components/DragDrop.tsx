345af5ea95168403b55771081c339505174cdddd
Karolis <urbaitiskarolis1@gmail.com>
2022-03-22 17:08:17 +0200

    refactor, start preparing for multi column drag and drop

--- components/DragDrop.tsx
+++ components/DragDrop.tsx
@@ -3,25 +3,30 @@ import {
   Button,
   Checkbox,
   createStyles,
+  Grid,
   Group,
   Modal,
   Text,
   Textarea,
   TextInput,
 } from "@mantine/core";
-import { useListState } from "@mantine/hooks";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { useNotifications } from "@mantine/notifications";
 import { Check, ExclamationMark } from "tabler-icons-react";
-import { uuid } from "uuidv4";
+import useSWR from "swr";
+import fetcher from "@utils/fetcher";
+import { createTask, updateTaskIndex } from "@lib/db";
+import { v4 as uuid } from "uuid";
+import { useListState } from "@mantine/hooks";
 
-type ListProps = {
-  data: {
-    id: string;
-    name: string;
-    description: string;
-    createdAt: number;
-  }[];
+type Task = {
+  description: string;
+  name: string;
+  createdAt: number;
+  column: number;
+  index: number;
+  completed: boolean;
+  id: string;
 };
 
 const useStyles = createStyles((theme) => ({
@@ -45,14 +50,29 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const DragDrop = ({ data }: ListProps) => {
+const DragDrop = () => {
   const { classes, cx } = useStyles();
   const notifications = useNotifications();
-  const [state, handlers] = useListState(data);
+  const { data } = useSWR("/api/tasks", fetcher);
+  const [state, handlers] = useListState<Task>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [addTaskModalOpened, setAddTaskModalOpened] = useState(false);
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
-  const addTask = (name: string, description: string) => {
+  const [activeColumn, setActiveColumn] = useState(0);
+  const [lastIndex, setLastIndex] = useState(0);
+  if (!data) return <>Loading...</>;
+  if (!tasks.length) {
+    new Promise((resolve) => {
+      resolve("Data passed");
+    }).then(() => {
+      setTasks(data.tasks);
+      handlers.setState(data.tasks);
+      setLastIndex(Math.max(...data.tasks.map((task: Task) => task.index), 0));
+    });
+  }
+  const columns = Math.max(...tasks.map((task: Task) => task.column), 0) + 1;
+  const addTask = async (name: string, description: string, column: number) => {
     if (!name.length)
       return notifications.showNotification({
         title: "No name",
@@ -67,9 +87,22 @@ const DragDrop = ({ data }: ListProps) => {
         color: "red",
         icon: <ExclamationMark />,
       });
-    handlers.append({ id: uuid(), name, description, createdAt: Date.now() });
+    //   handlers.append({ id: uuid(), name, description, createdAt: Date.now() });
+    const newTask: Task = {
+      name: taskName,
+      description: taskDescription,
+      index: lastIndex + 1,
+      column: column,
+      completed: false,
+      createdAt: Date.now(),
+      id: uuid(),
+    };
+    await createTask(newTask);
+    setTasks([...tasks, newTask]);
     setTaskName("");
     setTaskDescription("");
+    setActiveColumn(0);
+    setLastIndex(lastIndex + 1);
     setAddTaskModalOpened(false);
     return notifications.showNotification({
       title: "Task added",
@@ -80,50 +113,116 @@ const DragDrop = ({ data }: ListProps) => {
   };
   return (
     <>
-      <DragDropContext
-        onDragEnd={({ destination, source }) =>
-          handlers.reorder({ from: source.index, to: destination?.index || 0 })
-        }
-      >
-        <Droppable droppableId="dnd-list" direction="vertical">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {state.map((item, idx) => (
-                <Draggable key={item.id} index={idx} draggableId={item.id}>
-                  {(provided, snapshot) => (
-                    <div
-                      className={cx(classes.item, {
-                        [classes.itemDragging]: snapshot.isDragging,
-                      })}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                    >
-                      <Checkbox color="blue" size="xs" mr={12} />
-                      <div>
-                        <Text>{item.name}</Text>
-                        <Text
-                          color="dimmed"
-                          size="sm"
-                          style={{
-                            display: "-webkit-box",
-                            WebkitLineClamp: 2,
-                            WebkitBoxOrient: "vertical",
-                            overflow: "hidden",
-                          }}
+      <Grid>
+        {[...Array(columns)].map((_, idx: number) => (
+          <Grid.Col key={idx} span={3}>
+            <DragDropContext
+              onDragEnd={
+                async ({ destination, source }) => {
+                  await updateTaskIndex(
+                    tasks.find(
+                      (task) =>
+                        task.index ===
+                        source.index +
+                          tasks.filter((task) => task.column !== idx).length
+                    )?.id || "",
+                    (destination?.index || 0) +
+                      tasks.filter((task) => task.column !== idx).length
+                  );
+                  await updateTaskIndex(
+                    tasks.find(
+                      (task) =>
+                        task.index ===
+                        (destination?.index || 0) +
+                          tasks.filter((task) => task.column !== idx).length
+                    )?.id || "",
+                    source.index +
+                      tasks.filter((task) => task.column !== idx).length
+                  );
+                  handlers.reorder({
+                    from: source.index,
+                    to: destination?.index || 0,
+                  });
+                  setTasks(state);
+                  console.log(state);
+                }
+                // console.log(source.index, destination?.index)
+              }
+            >
+              <Droppable droppableId="dnd-list" direction="vertical">
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {state
+                      .filter((task: Task) => task.column === idx)
+                      //   .sort((a: Task, b: Task) => a.index - b.index)
+                      .map((task: Task, idx: number) => (
+                        <Draggable
+                          key={task.id}
+                          index={idx}
+                          draggableId={task.id}
                         >
-                          {item.description}
-                        </Text>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+                          {(provided, snapshot) => (
+                            <div
+                              className={cx(classes.item, {
+                                [classes.itemDragging]: snapshot.isDragging,
+                              })}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}
+                            >
+                              <Checkbox color="blue" size="xs" mr={12} />
+                              <div>
+                                <Text>{task.name}</Text>
+                                <Text
+                                  color="dimmed"
+                                  size="sm"
+                                  style={{
+                                    display: "-webkit-box",
+                                    WebkitLineClamp: 2,
+                                    WebkitBoxOrient: "vertical",
+                                    overflow: "hidden",
+                                  }}
+                                >
+                                  {task.description}
+                                </Text>
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+            <Button
+              variant="subtle"
+              color="dark"
+              fullWidth
+              radius="md"
+              onClick={() => {
+                setAddTaskModalOpened(true);
+                setActiveColumn(idx);
+              }}
+              styles={(theme) => ({
+                root: {
+                  transition: "all .2s",
+                  "&:hover": {
+                    backgroundColor:
+                      theme.colorScheme === "dark"
+                        ? theme.fn.darken(theme.colors.dark[5], 0.05)
+                        : theme.fn.lighten(theme.colors.gray[1], 0.05),
+                    color:
+                      theme.colorScheme === "dark" ? theme.white : theme.black,
+                  },
+                },
+              })}
+            >
+              + Add task
+            </Button>
+          </Grid.Col>
+        ))}
+      </Grid>
       <Modal
         opened={addTaskModalOpened}
         onClose={() => setAddTaskModalOpened(false)}
@@ -152,33 +251,12 @@ const DragDrop = ({ data }: ListProps) => {
         <Group position="right" mt="md">
           <Button
             variant="light"
-            onClick={() => addTask(taskName, taskDescription)}
+            onClick={() => addTask(taskName, taskDescription, activeColumn)}
           >
             Add
           </Button>
         </Group>
       </Modal>
-      <Button
-        variant="subtle"
-        color="dark"
-        fullWidth
-        radius="md"
-        onClick={() => setAddTaskModalOpened(true)}
-        styles={(theme) => ({
-          root: {
-            transition: "all .2s",
-            "&:hover": {
-              backgroundColor:
-                theme.colorScheme === "dark"
-                  ? theme.fn.darken(theme.colors.dark[5], 0.05)
-                  : theme.fn.lighten(theme.colors.gray[1], 0.05),
-              color: theme.colorScheme === "dark" ? theme.white : theme.black,
-            },
-          },
-        })}
-      >
-        + Add task
-      </Button>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,8 @@ import Brand from "@components/Brand";
 import DragDrop from "@components/DragDrop";
 import Links from "@components/Links";
 import User from "@components/User";
-import { AppShell, Grid, Group, Navbar } from "@mantine/core";
+import { AppShell, Navbar } from "@mantine/core";
 import type { NextPage } from "next";
-import { uuid } from "uuidv4";
-import { firestore } from "@firebase/clientApp";
 
 const NavBar = () => {
   return (
@@ -26,70 +24,7 @@ const NavBar = () => {
 const Home: NextPage = () => {
   return (
     <AppShell navbar={<NavBar />}>
-      <Grid>
-        <Grid.Col span={3}>
-          <Group direction="column" position="center" spacing={0}>
-            <DragDrop
-              data={[
-                {
-                  id: uuid(),
-                  name: "New task",
-                  description:
-                    "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                  createdAt: Date.now(),
-                },
-                {
-                  id: uuid(),
-                  name: "New task2",
-                  description:
-                    "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                  createdAt: Date.now(),
-                },
-              ]}
-            />
-          </Group>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <DragDrop
-            data={[
-              {
-                id: uuid(),
-                name: "New task",
-                description:
-                  "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                createdAt: Date.now(),
-              },
-              {
-                id: uuid(),
-                name: "New task2",
-                description:
-                  "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                createdAt: Date.now(),
-              },
-            ]}
-          />
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <DragDrop
-            data={[
-              {
-                id: uuid(),
-                name: "New task",
-                description:
-                  "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                createdAt: Date.now(),
-              },
-              {
-                id: uuid(),
-                name: "New task2",
-                description:
-                  "Add ability for service owners to invite employees to join their service with a custom generated one-time link with expiration date or if employee has their user already created just to invite them",
-                createdAt: Date.now(),
-              },
-            ]}
-          />
-        </Grid.Col>
-      </Grid>
+      <DragDrop />
     </AppShell>
   );
 };
