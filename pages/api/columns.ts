6e0ffae9163a1cc00b8cd4eb50ad16a87c4cf146
Karolis <urbaitiskarolis1@gmail.com>
2022-03-24 17:37:52 +0200

    properly get columns with tasks

--- pages/api/columns.ts
+++ pages/api/columns.ts
@@ -1,9 +1,16 @@
-import { getColumns } from "@lib/db";
+import { getColumns, getTasks } from "@lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const result = async (_: NextApiRequest, res: NextApiResponse) => {
   try {
     const { columns } = await getColumns();
+    if (columns)
+      await Promise.all(
+        columns?.map(async (column) => {
+          const { tasks } = await getTasks(column.id);
+          column.cards = tasks;
+        })
+      );
     res.status(200).json({ columns });
   } catch (error) {
     res.status(500).json({ error });
