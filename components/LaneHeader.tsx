123f4c8f1d1ee3a8806705b05ba4a0e0a7778a0f
Karolis <urbaitiskarolis1@gmail.com>
2022-04-19 11:40:07 +0300

    add delete column button

--- components/LaneHeader.tsx
+++ components/LaneHeader.tsx
@@ -1,7 +1,20 @@
-import { Button, Grid, Group, Text } from "@mantine/core";
-import { DotsVertical, Plus } from "tabler-icons-react";
+import { deleteColumn, findColumnIdByName } from "@lib/db";
+import { Grid, Menu, Text } from "@mantine/core";
+import { useNotifications } from "@mantine/notifications";
+import { Check, Pencil, Trash } from "tabler-icons-react";
 
 const LaneHeader = ({ title }) => {
+  const notifications = useNotifications();
+  const deleteLane = async () => {
+    const columnId = await findColumnIdByName(title);
+    await deleteColumn(columnId);
+    return notifications.showNotification({
+      title: "Column deleted",
+      message: "Your column was succesfully deleted",
+      color: "green",
+      icon: <Check />,
+    });
+  };
   return (
     <Grid
       style={{ cursor: "grab", width: 300, marginLeft: 5 }}
@@ -9,52 +22,25 @@ const LaneHeader = ({ title }) => {
       justify="space-between"
     >
       <Text weight={600}>{title}</Text>
-      <Group spacing="xs">
-        <Button
-          variant="subtle"
-          color="dark"
-          size="lg"
-          compact
-          radius="md"
-          p={0}
-          styles={(theme) => ({
-            root: {
-              transition: "all .2s",
-              "&:hover": {
-                backgroundColor:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[7]
-                    : theme.white,
-                color: theme.colorScheme === "dark" ? theme.white : theme.black,
-              },
+      <Menu
+        sx={(theme) => ({
+          button: {
+            transition: "all .2s",
+            "&:hover": {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[7]
+                  : theme.white,
+              color: theme.colorScheme === "dark" ? theme.white : theme.black,
             },
-          })}
-        >
-          <Plus />
-        </Button>
-        <Button
-          variant="subtle"
-          color="dark"
-          size="lg"
-          compact
-          radius="md"
-          p={0}
-          styles={(theme) => ({
-            root: {
-              transition: "all .2s",
-              "&:hover": {
-                backgroundColor:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[7]
-                    : theme.white,
-                color: theme.colorScheme === "dark" ? theme.white : theme.black,
-              },
-            },
-          })}
-        >
-          <DotsVertical />
-        </Button>
-      </Group>
+          },
+        })}
+      >
+        <Menu.Item icon={<Pencil size={14} />}>Edit column title</Menu.Item>
+        <Menu.Item color="red" icon={<Trash size={14} />} onClick={deleteLane}>
+          Delete column
+        </Menu.Item>
+      </Menu>
     </Grid>
   );
 };
