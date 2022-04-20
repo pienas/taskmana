ff088dd95965753b84dc55c366efc7411e3c6b98
Karolis <urbaitiskarolis1@gmail.com>
2022-04-20 14:04:37 +0300

    add logout

--- components/User.tsx
+++ components/User.tsx
@@ -1,16 +1,20 @@
+import { auth } from "@lib/firebase";
 import {
   Avatar,
   Box,
   Group,
+  Menu,
   Text,
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core";
-import { ChevronRight } from "tabler-icons-react";
+import { useRouter } from "next/router";
+import { ChevronRight, Logout } from "tabler-icons-react";
 import { useAuth } from "./AuthProvider";
 
 const User = () => {
   const theme = useMantineTheme();
+  const router = useRouter();
   const { displayName, email, photoURL } = useAuth();
   return (
     <Box
@@ -23,36 +27,55 @@ const User = () => {
         }`,
       }}
     >
-      <UnstyledButton
-        sx={{
-          display: "block",
-          width: "100%",
-          padding: theme.spacing.xs,
-          borderRadius: theme.radius.sm,
-          color:
-            theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+      <Menu
+        sx={{ width: "100%" }}
+        position="right"
+        placement="center"
+        control={
+          <UnstyledButton
+            sx={{
+              display: "block",
+              width: "100%",
+              padding: theme.spacing.xs,
+              borderRadius: theme.radius.sm,
+              color:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[0]
+                  : theme.black,
 
-          "&:hover": {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[6]
-                : theme.colors.gray[0],
-          },
-        }}
+              "&:hover": {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[6]
+                    : theme.colors.gray[0],
+              },
+            }}
+          >
+            <Group>
+              <Avatar src={photoURL} radius="xl" />
+              <Box sx={{ flex: 1 }}>
+                <Text size="sm" weight={500}>
+                  {displayName}
+                </Text>
+                <Text color="dimmed" size="xs">
+                  {email}
+                </Text>
+              </Box>
+              <ChevronRight size={18} />
+            </Group>
+          </UnstyledButton>
+        }
       >
-        <Group>
-          <Avatar src={photoURL} radius="xl" />
-          <Box sx={{ flex: 1 }}>
-            <Text size="sm" weight={500}>
-              {displayName}
-            </Text>
-            <Text color="dimmed" size="xs">
-              {email}
-            </Text>
-          </Box>
-          <ChevronRight size={18} />
-        </Group>
-      </UnstyledButton>
+        <Menu.Item
+          icon={<Logout size={14} />}
+          onClick={() => {
+            router.replace("/");
+            auth.signOut();
+          }}
+        >
+          Logout
+        </Menu.Item>
+      </Menu>
     </Box>
   );
 };
